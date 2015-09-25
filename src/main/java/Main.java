76df4a60b45fe3841ddcd23f7f4e80a433291c6c
Brian Gathright <bgathright@luc.edu>
2015-09-25 17:20:44 -0500

    adding servlet for heroku

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import org.apache.log4j.BasicConfigurator;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+
+public class Main extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("hello world");
+    }
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        BasicConfigurator.configure();
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new Main()),"/*");
+        server.start();
+        server.join();
+    }
+}
--- src/main/java/webservicetwo/webservicetwo/Client.java
+++ src/main/java/webservicetwo/webservicetwo/Client.java
@@ -4,7 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Main 
+public class Client 
 {
 	public static void main(String[] args)
 	{
