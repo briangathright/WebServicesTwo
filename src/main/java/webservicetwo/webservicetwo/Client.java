94921f950dbd3296d611f8de467d110bda895f0f
Brian Gathright <bgathright@luc.edu>
2015-09-26 13:45:18 -0500

    testing retrieve

--- src/main/java/webservicetwo/webservicetwo/Client.java
+++ src/main/java/webservicetwo/webservicetwo/Client.java
@@ -42,13 +42,13 @@ public class Client
 		
 		Product retP1 = dao.retrieveProduct(p1, p1.getID());
 		writer.println("retrieved product is: " + retP1.getID() + " " + retP1.getDetail());
-//		Customer retC1 = dao.retrieveCustomer(c1, (long)1);
-//		
-//		writer.println("retreived customer is: " + retC1.getID() + " " + retC1.getName());
-// 		writer.println("Retrieved products: ");
-//		for(Product p : retC1.getProductList())
-//		{
-//			System.out.println(p.getDetail());
-//		}
+		Customer retC1 = dao.retrieveCustomer(c1, (long)1);
+		
+		writer.println("retreived customer is: " + retC1.getID() + " " + retC1.getName());
+ 		writer.println("Retrieved products: ");
+		for(Product p : retC1.getProductList())
+		{
+			System.out.println(p.getDetail());
+		}
 	}
 }
