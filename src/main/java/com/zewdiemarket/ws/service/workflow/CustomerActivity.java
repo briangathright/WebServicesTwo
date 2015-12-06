ba3b2150c4ff44035f8029a471e1cc4ebe654757
Brian Gathright <bgathright.luc.edu>
2015-12-06 14:06:28 -0600

    fixed link

--- src/main/java/com/zewdiemarket/ws/service/workflow/CustomerActivity.java
+++ src/main/java/com/zewdiemarket/ws/service/workflow/CustomerActivity.java
@@ -69,7 +69,7 @@ public class CustomerActivity {
 		Link[] links = new Link[6];
 		Link updatePass = new Link("update_password", System.getenv("CUSTOMERSERVICE_URL") + customerRep.getID() + "?password=");
 		Link updateAddr = new Link("update_address", System.getenv("CUSTOMERSERVICE_URL") + customerRep.getID() + "?address=");
-		Link billingInfo = new Link("view_billingInfo", System.getenv("BILLINGINFO_URL") + customerRep.getBillingInfoID());
+		Link billingInfo = new Link("view_billingInfo", System.getenv("BILLINGINFOSERVICE_URL") + customerRep.getBillingInfoID());
 		Link orders = new Link("view_orders", System.getenv("ORDERSERVICE_URL")+ "customerorders/" + customerRep.getID());
 		Link cancel_order = new Link("cancel_order", System.getenv("ORDERSERVICE_URL)" + "cancel/"));
 		Link search = new Link("search", System.getenv("PRODUCTSERVICE_URL"));
