f0eca50402654a4cd741d45705fb349bdf9b5262
Brian Gathright <bgathright.luc.edu>
2015-12-06 14:01:20 -0600

    added some links

--- src/main/java/com/zewdiemarket/ws/service/workflow/CustomerActivity.java
+++ src/main/java/com/zewdiemarket/ws/service/workflow/CustomerActivity.java
@@ -66,18 +66,19 @@ public class CustomerActivity {
 	}
 
 	public void setLinks(CustomerRepresentation customerRep){
-		Link[] links = new Link[5];
+		Link[] links = new Link[6];
 		Link updatePass = new Link("update_password", System.getenv("CUSTOMERSERVICE_URL") + customerRep.getID() + "?password=");
 		Link updateAddr = new Link("update_address", System.getenv("CUSTOMERSERVICE_URL") + customerRep.getID() + "?address=");
 		Link billingInfo = new Link("view_billingInfo", System.getenv("BILLINGINFO_URL") + customerRep.getBillingInfoID());
-		Link orders = new Link("view_orders", System.getenv("ORDERSERVICE_URL"+ "customerorders/" + customerRep.getID()));
-		Link cancel_order = new Link("cancel_order", System.getenv("ORDERSERVICE_URL" + "cancel/"));
+		Link orders = new Link("view_orders", System.getenv("ORDERSERVICE_URL")+ "customerorders/" + customerRep.getID());
+		Link cancel_order = new Link("cancel_order", System.getenv("ORDERSERVICE_URL)" + "cancel/"));
 		Link search = new Link("search", System.getenv("PRODUCTSERVICE_URL"));
 		links[0] = orders;
 		links[1] = updatePass;
 		links[2] = updateAddr;
 		links[3] = cancel_order;
 		links[4] = search;
+		links[5] = billingInfo;
 		customerRep.setLinks(links);
 	}
 
